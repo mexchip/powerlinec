853fd4f9bf2445a9753a5d40f66ce30712d8c9df
José Jorge <josejorge@gmail.com>
2018-06-15 01:52:05 -0500

    Prints an icon for every commit ahead/behind origin remote

--- segment_git.c
+++ segment_git.c
@@ -10,9 +10,12 @@ typedef struct {
 	uint8_t dirty;
 	uint8_t modified;
 	uint8_t staged;
+	uint8_t ahead;
+	uint8_t behind;
 } GIT_STATUS;
 
 static int get_git_status(GIT_STATUS* git_status);
+static git_commit* branch_to_commit(git_repository* repo, git_reference* branch);
 
 int segment_git(SEGMENT* segment) {
 	int error = 0;
@@ -32,17 +35,32 @@ int segment_git(SEGMENT* segment) {
 			length += strlen(icons[MODIFIED]);
 		if (status.staged)
 			length += strlen(icons[STAGED]);
+
+		length += status.ahead * strlen(icons[AHEAD]);
+		length += status.behind * strlen(icons[BEHIND]);
 		
 		segment->text = (char*)malloc(length);
 		memset(segment->text, 0, length);
-		sprintf(
-			segment->text, "%s %s%s%s%s",
-			icons[GIT],
-			status.branch_name,
-			(status.modified || status.staged)? " " : "",
-			status.modified ? icons[MODIFIED] : "",
-			status.staged ? icons[STAGED] : ""
-		);
+
+		strcpy(segment->text, icons[GIT]);
+		for (int i = 0; i < status.ahead; i++) {
+			strcat(segment->text, icons[AHEAD]);
+		}
+		for (int i = 0; i < status.behind; i++) {
+			strcat(segment->text, icons[BEHIND]);
+		}
+
+		strcat(segment->text, " ");
+		strcat(segment->text, status.branch_name);
+
+		if (status.modified || status.staged)
+			strcat(segment->text, " ");
+
+		if (status.modified)
+			strcat(segment->text, icons[MODIFIED]);
+		
+		if (status.staged)
+			strcat(segment->text, icons[STAGED]);
 
 		if (status.dirty > 0) {
 			segment->background++;
@@ -64,6 +82,33 @@ static int get_git_status(GIT_STATUS* git_status) {
 		git_reference* head = NULL;
 		error = git_repository_head(&head, repo);
 		if (0 == error) {
+			git_remote* remote = NULL;
+			error = git_remote_lookup(&remote, repo, "origin");
+			if (0 == error) {
+				const char* fetch = git_remote_url(remote);
+				if (fetch) {
+					// printf("Fetch %s\n", fetch);
+					git_reference* upstream;
+					error = git_branch_upstream(&upstream, head);
+					if (0 == error) {
+						// printf("no error upstream\n");
+						size_t ahead, behind;
+						git_commit* branch_commit = branch_to_commit(repo, head);
+						git_commit* upstream_commit = branch_to_commit(repo, upstream);;
+						const git_oid* local_oid;
+						const git_oid* upstream_oid;
+						local_oid = git_commit_id(branch_commit);
+						upstream_oid = git_commit_id(upstream_commit);
+						git_graph_ahead_behind(&ahead, &behind, repo, local_oid, upstream_oid);
+						// printf("ahead: %lu, behind: %lu\n", ahead, behind);
+						git_status->ahead = ahead;
+						git_status->behind = behind;
+					}
+					else {
+						// printf("error upstream\n");
+					}
+				}
+			}
 			git_status->branch_name = strdup(git_reference_shorthand(head));
 			git_status_options options = GIT_STATUS_OPTIONS_INIT;
 			options.show = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
@@ -116,4 +161,29 @@ static int get_git_status(GIT_STATUS* git_status) {
 	git_libgit2_shutdown();
 
 	return -1;
+}
+
+git_commit* branch_to_commit(git_repository* repo, git_reference* branch) {
+	int error = 0;
+
+	git_object* commit_obj;
+	error = git_reference_peel(&commit_obj, branch, GIT_OBJ_COMMIT);
+	if (0 != error) {
+		// printf("error peel\n");
+		return NULL;
+	}
+
+	const git_oid* oid = git_object_id(commit_obj);
+
+	git_commit* commit;
+	error = git_commit_lookup(&commit, repo, oid);
+	git_object_free(commit_obj);
+
+	if (0 == error) {
+		// printf("returning commit\n");
+		return commit;
+	}
+
+	// printf("returning NULL\n");
+	return NULL;
 }
